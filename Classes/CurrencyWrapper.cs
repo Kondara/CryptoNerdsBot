54cc7a655e75322e257280232d958de2f9b5348b
Kondara <complexityx@hotmail.com>
2017-08-05 16:38:25 -0700

    Add files via upload

---
+++ Classes/CurrencyWrapper.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telegram_Bot.Classes
+{
+    public struct CryptoCurrency
+    {
+        private readonly string _currencyName, _marketName, _apiName;
+
+        public CryptoCurrency(string currencyName, string marketName, string apiName)
+        {
+            this._currencyName = currencyName;
+            this._marketName = marketName;
+            this._apiName = apiName;
+        }
+
+        public string currencyName { get { return _currencyName; } }
+        public string marketName { get { return _marketName; } }
+        public string apiName { get { return _apiName; } }
+    }
+
+    class CurrencyWrapper
+    {
+
+        internal List<CryptoCurrency> supportedCurrencies = new List<CryptoCurrency>();
+
+        public CurrencyWrapper()
+        {
+            SetSupportedCurrencies();
+        }
+
+        public string GetCurrencyPrice(string currencyLookup)
+        {
+            /* Lookup Currency Name */
+            CryptoCurrency Currency = supportedCurrencies.Find(x => x.currencyName == currencyLookup);
+
+            if (Currency.currencyName != null)
+            {
+                switch (Currency.apiName)
+                {
+                    case "GDAX":
+                        return GetGDAXTicker(Currency.marketName);
+                    case "CoinMarket":
+                        return GetCoinMarketTicker(Currency.marketName);
+                    case "Bittrex":
+                        return "Not yet implemented.";
+                    default:
+                        return "Could not find a suitable API.";
+                }
+            }
+            else
+            {
+                return "Currency not found.";
+            }
+
+        }
+
+        internal string GetGDAXTicker(string marketName)
+        {
+            string TickerUrl = "https://api.gdax.com/products/" + marketName + "/ticker";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(TickerUrl);
+            request.ContentType = "text/html";
+            request.UserAgent = "CryptoNerdBot";
+            request.Method = "GET";
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                var rootResult = JsonConvert.DeserializeObject<GDAXProductTicker>(reader.ReadToEnd());
+                return rootResult.price;
+            }
+        }
+
+        internal string GetCoinMarketTicker(string marketName)
+        {
+            string TickerUrl = "https://api.coinmarketcap.com/v1/ticker/" + marketName + "/";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(TickerUrl);
+            request.ContentType = "text/html";
+            request.UserAgent = "CryptoNerdBot";
+            request.Method = "GET";
+
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                var rootResult = JsonConvert.DeserializeObject<List<CoinMarketTicker>>(reader.ReadToEnd());
+                return rootResult.First().price_usd;
+            }
+        }
+
+        internal void SetSupportedCurrencies()
+        {
+            supportedCurrencies.Add(new CryptoCurrency("ETH", "ETH-USD", "GDAX"));
+            supportedCurrencies.Add(new CryptoCurrency("ETH-USD", "ETH-USD", "GDAX"));
+            supportedCurrencies.Add(new CryptoCurrency("ETH-BTC", "ETH-BTC", "GDAX"));
+            supportedCurrencies.Add(new CryptoCurrency("BTC", "BTC-USD", "GDAX"));
+            supportedCurrencies.Add(new CryptoCurrency("BTC-ETH", "ETH-BTC", "GDAX"));
+            supportedCurrencies.Add(new CryptoCurrency("BTC-LTC", "LTC-BTC", "GDAX"));
+            supportedCurrencies.Add(new CryptoCurrency("LTC", "LTC-USD", "GDAX"));
+            supportedCurrencies.Add(new CryptoCurrency("LTC-BTC", "LTC-BTC", "GDAX"));
+            supportedCurrencies.Add(new CryptoCurrency("BCH", "bitcoin-cash", "CoinMarket"));
+            supportedCurrencies.Add(new CryptoCurrency("BCC", "bitcoin-cash", "CoinMarket")); // Referring to BitCoin Cash for now.
+            supportedCurrencies.Add(new CryptoCurrency("LBRY", "library-credit", "CoinMarket"));
+            supportedCurrencies.Add(new CryptoCurrency("LBC", "library-credit", "CoinMarket"));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.ServiceModel.Web;
+
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineKeyboardButtons;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.InputMessageContents;
+using Telegram.Bot.Types.ReplyMarkups;
+
+using Newtonsoft.Json;
+using Telegram_Bot.Classes;
+
+
+namespace Telegram_Bot
+{
+    class Program
+    {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient(ConfigurationManager.AppSettings["APIKey"]);
+        private static CurrencyWrapper currencyWrapper = new CurrencyWrapper();
+
+        static void Main(string[] args)
+        {
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+            Bot.OnMessage += BotOnMessageReceived;
+            Bot.OnMessageEdited += BotOnMessageReceived;
+            Bot.OnReceiveError += BotOnReceiveError;
+
+            var me = Bot.GetMeAsync().Result;
+
+            Console.Title = me.Username;
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            Debugger.Break();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            string currency;
+            string priceMessage;
+
+            Console.WriteLine("Received Message from {0}", message.Chat.Id);
+            Console.WriteLine(message.Text);
+
+            if (message == null || message.Type != MessageType.TextMessage) return;
+
+            if (message.Text.StartsWith("/insultjohn"))
+            {
+                await Bot.SendTextMessageAsync(message.Chat.Id, "I wasn't born with enough middle fingers to let you know how I feel about John.");
+            }
+            else if (message.Text.StartsWith("/insultjason"))
+            {
+                await Bot.SendTextMessageAsync(message.Chat.Id, "Why would I do that? Jason is a big teddy bear. I fucking love teddy bears.");
+            }
+            else if (message.Text.StartsWith("/getprice"))
+            {
+                currency = message.Text.Remove(0, message.Text.IndexOf(' ') + 1);
+                priceMessage = currencyWrapper.GetCurrencyPrice(currency.ToUpper());
+
+                await Bot.SendTextMessageAsync(message.Chat.Id, priceMessage);
+            }
+            else
+            {
+                string usage = "Commands: \n/getprice <currency accronym> \n/insultjohn \n/insultjason";
+
+                await Bot.SendTextMessageAsync(message.Chat.Id, usage);
+            }
+
+        }
+
+        private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
+        {
+            await Bot.AnswerCallbackQueryAsync(callbackQueryEventArgs.CallbackQuery.Id,
+                $"Received {callbackQueryEventArgs.CallbackQuery.Data}");
+        }
+
+    }
+
+}
